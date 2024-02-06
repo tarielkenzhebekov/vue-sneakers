c030b8b075ccb657a25ea95bf2e8f08bb010fb10
Tariel <tarielkenzhebekov@gmail.com>
2024-02-06 23:08:32 +0600

    Added order complete message

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <script setup>
-import { ref, reactive, watch, provide, computed } from 'vue'
-import axios from 'axios'
+import { ref, watch, provide, computed } from 'vue'
 import Header from './components/Header.vue'
 import Drawer from './components/Drawer.vue'
 
 const cart = ref([]);
-
-const isCreatingOrder = ref(false);
 
 const totalPrice = computed(
   () => cart.value.reduce((acc, item) => acc + item.price, 0)
@@ -14,10 +11,6 @@ const totalPrice = computed(
 
 const vatPrice = computed(
   () => Math.round(totalPrice.value * 0.05)
-);
-
-const cartButtonDisabled = computed(
-  () => isCreatingOrder.value ? true : totalPrice.value ? false : true
 );
 
 const drawerOpen = ref(false);
@@ -40,24 +33,6 @@ const removeFromCart = (item) => {
   item.isAdded = false;
 }
 
-const createOrder = async () => {
-  try {
-    isCreatingOrder.value = true;
-    const { data } = await axios.post(`https://982d905e50f84a79.mokky.dev/orders`, {
-      items: cart.value,
-      totalPrice: totalPrice.value
-    });
-
-    cart.value = [];
-
-    return data;
-  } catch(err) {
-    console.log(err);
-  } finally {
-    isCreatingOrder.value = false;
-  }
-}
-
 watch(cart, () => {
     localStorage.setItem('cart', JSON.stringify(cart.value))
   },
@@ -78,8 +53,6 @@ provide('cart', {
 <Drawer v-if="drawerOpen" 
   :total-price="totalPrice"
   :vat-price="vatPrice"
-  :button-disabled="cartButtonDisabled"
-  @create-order="createOrder"
 />
 
 <div class="bg-white w-4/5 m-auto rounded-2xl shadow-2xl mt-14">
--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,26 +1,60 @@
 <script setup>
+import axios from 'axios';
+import { inject, ref, computed } from 'vue';
 import DrawerHead from './DrawerHead.vue';
 import CartItemList from './CartItemList.vue';
 import InfoBlock from './InfoBlock.vue';
 
-defineProps({
+const props = defineProps({
     totalPrice: Number,
     vatPrice: Number,
-    buttonDisabled: Boolean
 });
 
-const emit = defineEmits(['createOrder']);
+const { cart } = inject('cart');
+
+const isCreatingOrder = ref(false);
+
+const orderId = ref(null);
+
+const buttonDisabled = computed(
+  () => isCreatingOrder.value ? true : props.totalPrice ? false : true
+);
+
+const createOrder = async () => {
+  try {
+    isCreatingOrder.value = true;
+    const { data } = await axios.post(`https://982d905e50f84a79.mokky.dev/orders`, {
+      items: cart.value,
+      totalPrice: props.totalPrice
+    });
+
+    cart.value = [];
+    orderId.value = data.id;
+  } catch(err) {
+    console.log(err);
+  } finally {
+    isCreatingOrder.value = false;
+  }
+}
+
 </script>
 <template>
 
 <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
 <div class="fixed top-0 right-0 h-full w-96 bg-white z-20 p-10">
     <DrawerHead />
-    <div v-if="!totalPrice" class="flex h-full items-center">
+    <div v-if="!totalPrice || orderId" class="flex h-full items-center">
         <InfoBlock
-        imageUrl="/package-icon.png"
-        title="Cart is empty"
-        description="Please, add at least one pair of sneakers to order"
+            v-if="!totalPrice && !orderId"
+            imageUrl="/package-icon.png"
+            title="Cart is empty"
+            description="Please, add at least one pair of sneakers to order"
+        />
+        <InfoBlock
+            v-if="orderId"
+            imageUrl="/order-success-icon.png"
+            title="Order is processed"
+            :description="`Your order #${orderId} will be transferred to courier delivery soon`"
         />
     </div>
     <div v-else>
@@ -37,7 +71,7 @@ const emit = defineEmits(['createOrder']);
             <b>${{ vatPrice }}</b>
         </div>
         <button 
-            v-on:click="emit('createOrder')"
+            v-on:click="createOrder"
             :disabled="buttonDisabled" 
             class="mt-3 bg-lime-500 w-full rounded-xl py-3 text-white 
                 disabled:bg-slate-300 hover:bg-lime-600 active:bg-lime-700 
